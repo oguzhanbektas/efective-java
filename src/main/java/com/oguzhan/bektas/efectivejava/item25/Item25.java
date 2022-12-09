999922f1d7c011cde7b94e166024fccfdee7060f
obektas <oguzhan.bektas@snitechnology.net>
2022-12-09 15:35:12 +0300

    item 25 add 1

---
+++ src/main/java/com/oguzhan/bektas/efectivejava/item25/Item25.java
@@ -0,0 +1,13 @@
+package com.oguzhan.bektas.efectivejava.item25;
+
+public class Item25 {
+
+    /*
+    Item25: Limit source files to a single top-level class
+    Birden fazla Sınıf ve arayüz tanımlarını tek dosya içinden yapma
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
