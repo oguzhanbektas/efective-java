6b1b1eaf7da86fefb7882a2df3cc3a9d4186ed88
obektas <oguzhan.bektas@snitechnology.net>
2022-12-09 15:35:05 +0300

    item 25 add

---
+++ src/main/java/com/oguzhan/bektas/efectivejava/item25/TestDrive.java
@@ -0,0 +1,14 @@
+package com.oguzhan.bektas.efectivejava.item25;
+
+public class TestDrive {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Utensil.NAME + Dessert.NAME);
+		
+		Test t = new Test();
+		t.yazdir();
+		
+	}
+
+}
---
+++ src/main/java/com/oguzhan/bektas/efectivejava/item25/Utensil.java
@@ -0,0 +1,10 @@
+package com.oguzhan.bektas.efectivejava.item25;
+
+//Two classes defined in one file. Don't ever do this!  (Page 115)
+class Utensil {
+	static final String NAME = "pan";
+}
+
+class Dessert {
+	static final String NAME = "cake";
+}
