bc8a106595b24cda52474ec3b86bbe631d7b23cb
obektas <oguzhan.bektas@snitechnology.net>
2023-01-17 10:52:30 +0300

    item 27 eliminate unchecked warnings

---
+++ src/main/java/com/oguzhan/bektas/efectivejava/item27/Item27.java
@@ -0,0 +1,11 @@
+package com.oguzhan.bektas.efectivejava.item27;
+
+public class Item27 {
+    /*
+    Eliminate unchecked warnings.
+    Genelde kod yazarken sarı vs görürüz .Bunların önüne nasıl geçeriz.
+    Bir cast operation yaptığımız zaman ben bunun doğruluğu kesin bilmiyorum hata olabilir diye warning veriyor
+       Sarılığı yok etmek için @SuppressWarnings("unchecked") yazabiliriz.
+
+     */
+}
